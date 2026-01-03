c12f8f1244c83eac282405d5bdbb9b0624e716b8
SayedMohammedYahiyaKhadri <sayedyahiya18@gmail.com>
2026-01-03 13:38:52 +0530

    Update fitness report test with new user data

--- test_app.py
+++ test_app.py
@@ -3,13 +3,13 @@ from fitness_app import fitness_report
 def test_fitness_report():
     expected_output = (
         "----- Fitness Report -----\n"
-        "Name            : Niranjan\n"
-        "Steps Taken     : 8000\n"
+        "Name            : Sayed\n"
+        "Steps Taken     : 7000\n"
         "Calories Burned : 500\n"
         "Workout Time    : 30 minutes\n"
         "Fitness Score   : 160.00\n"
         "Fitness Level   : Excellent"
     )
 
-    result = fitness_report("Niranjan", 8000, 500, 30)
+    result = fitness_report("Sayed", 7000, 500, 30)
     assert result == expected_output
