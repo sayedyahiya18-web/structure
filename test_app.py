99ea0f8ad7b4c3ea12ad0de1d1937eeb35ac155b
SayedMohammedYahiyaKhadri <sayedyahiya18@gmail.com>
2026-01-03 13:45:41 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,15 +1,15 @@
 from app import fitness_report
 
 def test_fitness_report():
-    expected_output = (
+    expected_output=(
         "----- Fitness Report -----\n"
-        "Name            : Sayed\n"
-        "Steps Taken     : 7000\n"
-        "Calories Burned : 500\n"
-        "Workout Time    : 30 minutes\n"
-        "Fitness Score   : 160.00\n"
-        "Fitness Level   : Excellent"
+        "Name:Sayed\n"
+        "Steps Taken:7000\n"
+        "Calories Burned:500\n"
+        "Workout Time:30 minutes\n"
+        "Fitness Score:160.00\n"
+        "Fitness Level:Excellent"
     )
 
-    result = fitness_report("Sayed", 7000, 500, 30)
-    assert result == expected_output
+    result=fitness_report("Sayed",7000,500,30)
+    assert result==expected_output
