fb10432fe0dc323f11a27412df141031e7b74c9a
SayedMohammedYahiyaKhadri <sayedyahiya18@gmail.com>
2026-01-03 13:47:28 +0530

    Refactor fitness_report formatting and calculation

--- app.py
+++ app.py
@@ -12,17 +12,17 @@ def calculate_fitness_level(score):
 
 
 def fitness_report(name, steps, calories, workout_time):
-    fitness_score = (steps / 100) + (calories / 10) + workout_time
-    level = calculate_fitness_level(fitness_score)
+    fitness_score=(steps/100)+(calories/10)+workout_time
+    level=calculate_fitness_level(fitness_score)
 
-    report = (
+    report =(
         "----- Fitness Report -----\n"
-        f"Name            : {name}\n"
-        f"Steps Taken     : {steps}\n"
-        f"Calories Burned : {calories}\n"
-        f"Workout Time    : {workout_time} minutes\n"
-        f"Fitness Score   : {fitness_score:.2f}\n"
-        f"Fitness Level   : {level}"
+        f"Name:{name}\n"
+        f"Steps Taken:{steps}\n"
+        f"Calories Burned:{calories}\n"
+        f"Workout Time:{workout_time} minutes\n"
+        f"Fitness Score:{fitness_score:.2f}\n"
+        f"Fitness Level:{level}"
     )
     return report
 
